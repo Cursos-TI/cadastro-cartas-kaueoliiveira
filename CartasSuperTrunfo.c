69842096946a21940b8c90fac4bfd1cd9e9aeafa
kaueoliiveira <kkaue.o.alves@gmail.com>
2026-03-03 22:54:32 +0000

    Adicionado a comparação e os super poderes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,22 +10,24 @@ int main() {
   char estado[50];
   char carta[50];
   char nome[50];
-  int populacao;
+  unsigned long int populacao;
   float area;
   float pib;
   int pontos_turisticos;
   float densidade_populacional;
   float pib_capita;
+  float super_poder;
     // cidade 2
   char estado2[50];
   char carta2[50];
   char nome2[50];
-  int populacao2;
+  unsigned long int populacao2;
   float area2;
   float pib2;
   int pontos_turisticos2;
   float densidade_populacional2;
   float pib_capita2;
+  float super_poder2;
 
 
 
@@ -38,7 +40,7 @@ int main() {
   printf("nome da cidade: ");
   scanf(" %[^\n]",nome);
   printf("população: ");
-  scanf("%d",&populacao);
+  scanf("%lu",&populacao);
   printf("area da cidade: ");
   scanf("%f",&area);
   printf("PIB da cidade: ");
@@ -57,7 +59,7 @@ int main() {
   printf("nome da cidade: ");
   scanf( " %[^\n]",nome2);
   printf("população: ");
-  scanf("%d",&populacao2);
+  scanf("%lu",&populacao2);
   printf("area da cidade: ");
   scanf("%f",&area2);
   printf("PIB da cidade: ");
@@ -69,33 +71,48 @@ int main() {
   densidade_populacional2 = (float) populacao2/area2;
   pib_capita2 = (float) pib2/populacao2;
 
+  //Área para calculos
+    //Cidade 1
+  super_poder = (float)populacao+area+pib+pontos_turisticos+pib_capita+1.0/densidade_populacional;
+    //Cidade 2
+  super_poder2 = (float)populacao2+area2+pib2+pontos_turisticos2+pib_capita2+1.0/densidade_populacional2;
 
   // Área para exibição dos dados da cidade
     //cidade 1
-printf("----- CIDADE 1 -----");
+printf("----- CIDADE 1 -----\n");
 printf("Letra da cidade 1:%s\n",estado);
 printf("Carta da cidade 1:%s\n",carta);
 printf("Nome da cidade 1:%s\n",nome);
-printf("População da cidade 1:%d\n",populacao);
+printf("População da cidade 1:%lu\n",populacao);
 printf("Area da cidade 1:%.2f\n",area);
-printf("PIB da cidade 1:%.2f\n",pib);
+printf("PIB da cidade 1 R$:%.2f\n",pib);
 printf("Pontos turisticos da cidade 1:%d\n",pontos_turisticos);
 
 
 printf("Densidade populacional da cidade 1:%.2f hab/km²\n",densidade_populacional);
 printf("PIB per capita da cidade 1:R$%.2f\n",pib_capita);
   // cidade 2
-printf("----- CIDADE 2 -----");
+printf("----- CIDADE 2 -----\n");
 printf("Letra da cidade 2:%s\n",estado2);
 printf("Carta da cidade 2:%s\n",carta2);
 printf("Nome da cidade 2:%s\n",nome2);
-printf("População da cidade 2:%d\n",populacao2);
+printf("População da cidade 2:%lu\n",populacao2);
 printf("Area da cidade 2:%.2f\n",area2);
-printf("PIB da cidade 2:%.2f\n",pib2);
+printf("PIB da cidade 2 R$:%.2f\n",pib2);
 printf("Pontos turisticos da cidade 2:%d\n",pontos_turisticos2);
 
 printf("Densidade populacional da cidade 2:%.2f hab/km²\n",densidade_populacional2);
 printf("PIB per capita da cidade 2:R$%.2f\n",pib_capita2);
+
+  // Comparação de cartas
+
+printf("população:%d\n",populacao>populacao2);
+printf("area:%d\n",area>area2);
+printf("Pib:%d\n",pib>pib2);
+printf("Pontos turisticos:%d\n",pontos_turisticos>pontos_turisticos2);
+printf("Pib per capita:%d\n",pib_capita>pib_capita2);
+printf("Densidade populacional:%d\n",densidade_populacional<densidade_populacional2);
+printf("Super Poder:%d\n",super_poder>super_poder2);
 
 return 0;
 } 
