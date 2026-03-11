74e4ff891fb2f6547f0b6f459aebbe4640f58d85
kaueoliiveira <kkaue.o.alves@gmail.com>
2026-03-11 00:34:50 +0000

    adicionado as comparações com o switch

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,7 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int escolha;
     // cidade 1
   char estado[50];
   char carta[50];
@@ -103,16 +104,69 @@ printf("Pontos turisticos da cidade 2:%d\n",pontos_turisticos2);
 
 printf("Densidade populacional da cidade 2:%.2f hab/km\n",densidade_populacional2);
 printf("PIB per capita da cidade 2:R$%.2f\n",pib_capita2);
-
-  // Comparação de cartas
-printf("Comparar PIB Per Capita\n");
-printf("Carta 1-%s R$:%.2f\n",nome,pib_capita);
-printf("Carta 2-%s R$:%.2f\n",nome2,pib_capita2);
-if (pib_capita == pib_capita2)
-  printf("Empate!\n");
-else if (pib_capita>pib_capita2)
-  printf("A carta 1 (%s) Ganhou! com diferença de R$:%.2f",nome,pib_capita-pib_capita2);
-else
-  printf("A carta 2 (%s) Ganhou! com diferença de R$:%.2f",nome2,pib_capita2-pib_capita);
+// Comparação de cartas
+printf("1- Nome\n2- Populção\n3- Área\n4- PIB\n5- Pontos turisticos\n6- Densidade geografica\n");
+printf("Digite o atributo a ser comparado:");
+scanf("%d",&escolha);
+switch (escolha)
+{
+case 1:
+  printf("Nome Cidade 1:%s\n",nome);
+  printf("Nome Cidade 2:%s\n",nome2);
+  break;
+case 2:
+  printf("População Cidade 1:%lu\n",populacao);
+  printf("População Cidade 2:%lu\n",populacao2);
+  if (populacao>populacao2)
+    printf("A Cidade %s Ganhou! Com diferença de %lu\n",nome,populacao-populacao2);
+  else if (populacao2>populacao)
+    printf("A Cidade %s Ganhou! Com diferença de %lu\n",nome2,populacao2-populacao);
+  else
+    printf("empate!\n");
+  break;
+case 3:
+  printf("Area Cidade 1:%.2f\n",area);
+  printf("Area Cidade 2:%.2f\n",area2);
+  if (area>area2)
+    printf("A Cidade %s Ganhou! Com diferença de %.2f\n",nome,area-area2);
+  else if (area2>area)
+    printf("A Cidade %s Ganhou! Com diferença de %.2f\n",nome2,area2-area);
+  else
+    printf("Empate!\n");
+  break;
+case 4:
+  printf("PIB Cidade 1 R$:%.2f\n",pib);
+  printf("PIB Cidade 2 R$:%.2f\n",pib2);
+  if (pib>pib2)
+    printf("A Cidade %s Ganhou! Com diferença de %.2f\n",nome,pib-pib2);
+  else if (pib2>pib)
+    printf("A Cidade %s Ganhou! Com diferença de %.2f\n",nome2,pib2-pib);
+  else
+    printf("Empate!\n");
+  break;
+case 5:
+  printf("Pontos turisticos Cidade 1:%d\n",pontos_turisticos);
+  printf("Pontos turisticos Cidade 2:%d\n",pontos_turisticos2);
+  if (pontos_turisticos>pontos_turisticos2)
+    printf("A Cidade %s Ganhou! Com diferença de %d\n",nome,pontos_turisticos-pontos_turisticos2);
+  else if (pontos_turisticos2>pontos_turisticos)
+    printf("A Cidade %s Ganhou! Com diferença de %d\n",nome2,pontos_turisticos2-pontos_turisticos);
+  else
+    printf("Empate!\n");
+  break;
+case 6:
+  printf("Densidade Populacional Cidade 1:%.2f\n",densidade_populacional);
+  printf("Densidade Populacional Cidade 2:%.2f\n",densidade_populacional2);
+  if (densidade_populacional<densidade_populacional2)
+    printf("A Cidade %s Ganhou! Com diferença de %.2f\n",nome,densidade_populacional2-densidade_populacional);
+  else if (densidade_populacional2<densidade_populacional)
+    printf("A Cidade %s Ganhou! Com diferença de %.2f\n",nome2,densidade_populacional-densidade_populacional2);
+  else
+    printf("Empate!\n");
+  break;
+default:
+  printf("Opção invalida!");
+  break;
+}
 return 0;
-} 
+}
